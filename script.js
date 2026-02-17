55aef469714004bff2e2e3e1770daab0c1232f3d
Mbae ali <mbaeali670@gmail.com>
2026-02-17 02:54:36 +0100

    commit 2

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let compteur = 0;
-
-const panierCount = document.getElementById("panier-count");
-const boutonsPanier = document.querySelectorAll(".btn-panier");
-
-boutonsPanier.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    compteur++;
-    panierCount.textContent = compteur;
+// PANIER
+let count = 0;
+const panier = document.querySelector('#compteur-panier');
+const btns = document.querySelectorAll('.btn-panier');
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    count++;
+    panier.textContent = `Panier (${count})`;
     alert("Excellent choix ! Ce produit a été ajouté.");
   });
 });
-document.addEventListener("DOMContentLoaded", function() {
-  const form= document.getElementById("formcontact");
-  if (form) {
-    form.addEventListener("submit", function(event) {
-      event.preventDefault() ;
 
-  alert("Merci! Mme Aicha a bien reçu votre message.");
-    });
-  }   
-});  
+// BOUTON RETOUR
+const btnTop = document.querySelector('#btn-top');
+window.addEventListener('scroll', () => {
+  btnTop.style.display = (window.scrollY > 200) ? 'block' : 'none';
+});
+btnTop.addEventListener('click', () => { window.scrollTo({ top:0, behavior:'smooth' }); });
+
+// FORMULAIRE CONTACT
+const form = document.querySelector('#contact-form');
+if(form){
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    alert("Merci ! Mme Aïcha a bien reçu votre message");
+    form.reset();
+  });
+}
+
